15714acf238d542246a64f75062effe2d358adcd
Michael Butt <mbutt@mitre-engenuity.org>
2022-02-08 16:25:54 -0500

    feat(infra): add terraform modules and network diagrams
    * add initial version of azure focused terraform modules
    * add initial version of auto-generated network diagrams for deployed infrastructure
    * add pre-commit terraform linting/formatting
    * augment gitignore with terraform-related settings
    * added current version of spreadsheet from sharepoint, att&ck navigator overlay

---
+++ infra/azure/tf/main.tf
@@ -0,0 +1,193 @@
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.0.0"
+    }
+  }
+}
+
+locals {
+  default_tags = {
+    "environment" = var.environment,
+    "name"        = var.name
+  }
+
+}
+
+# Reference to the current subscription.  Used when creating role assignments
+data "azurerm_subscription" "current" {}
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-keyvault_test_rg"
+  location = "eastus"
+
+  tags = local.default_tags
+
+}
+
+resource "azurerm_key_vault" "azvault" {
+  name                        = "${var.prefix}-keyvault-test1"
+  location                    = azurerm_resource_group.default.location
+  resource_group_name         = azurerm_resource_group.default.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  tags                        = local.default_tags
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+    ]
+
+    secret_permissions = [
+      "get",
+    ]
+
+    storage_permissions = [
+      "get",
+    ]
+  }
+}
+
+
+resource "azurerm_resource_group" "default" {
+  name     = "${var.name}-${var.environment}-rg"
+  location = var.location
+  tags     = local.default_tags
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  tags                = local.default_tags
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "${var.prefix}-win-vm"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  size                = "Standard_F2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  tags                = local.default_tags
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+resource "azurerm_network_interface" "main-linux" {
+  name                = "${var.prefix}-linux-nic"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  tags                = local.default_tags
+
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-linux-vm"
+  resource_group_name             = azurerm_resource_group.default.name
+  location                        = azurerm_resource_group.default.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main-linux.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  tags = local.default_tags
+
+  //  tags = merge(local.default_tags,{
+  //    Name = "Instance-${var.environment_name}${count.index}"
+  //    },)
+}
+
+resource "azurerm_container_group" "main" {
+  name                = "${var.prefix}-container-group"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  ip_address_type     = "public"
+  dns_name_label      = "${var.dns_prefix}-${var.environment}-container"
+  os_type             = "linux"
+
+  container {
+    name   = "hw"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "sidecar"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = local.default_tags
+}
---
+++ infra/azure/tf/variables.tf
@@ -0,0 +1,53 @@
+variable "name" {
+  type        = string
+  description = "Location of the azure resource group."
+  default     = "cloud-analytics"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of the deployment environment"
+  default     = "dev"
+}
+
+variable "location" {
+  type        = string
+  description = "Location to deploy the resoruce group"
+  default     = "East US"
+}
+
+variable "dns_prefix" {
+  type        = string
+  description = "A prefix for any dns based resources"
+  default     = "ca"
+}
+
+variable "plan_tier" {
+  type        = string
+  description = "The tier of app service plan to create"
+  default     = "Standard"
+}
+
+variable "plan_sku" {
+  type        = string
+  description = "The sku of app service plan to create"
+  default     = "S1"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for all resources"
+  default     = "ca"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "The admin username for the VMs"
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "The admin password for the VMs"
+  default     = "Pa$$w0rd1234"
+}
